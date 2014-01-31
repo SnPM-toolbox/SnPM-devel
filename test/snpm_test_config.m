cc329ce91e3e882c7490a8c4dc15a73d815b8164
cmaumet <c.m.j.maumet@warwick.ac.uk>
2014-01-31 10:53:52 +0000

    test: automatically recompute ground truth with SnPM13

---
+++ test/snpm_test_config.m
@@ -0,0 +1,15 @@
+% Configuration file for computing ground truth tests.
+%
+% Update this file to specify your data directory.
+%
+% In <testDataDir> you must find:
+% 5 subject directories named su_control01 to su_control05 containing each
+% 1 session directory named cn_sess1 with a single contrast file named 
+% con_0001.img.
+%_______________________________________________________________________
+% Copyright (C) 2013 The University of Warwick
+% Id: snpm_test_config.m  SnPM13 2013/10/12
+% Camille Maumet
+
+global testDataDir;
+testDataDir = ''; % Set path to your test data here
