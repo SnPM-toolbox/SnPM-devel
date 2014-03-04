97ec8905a26e1a9822be25f44302d0711939cce9
cmaumet <c.m.j.maumet@warwick.ac.uk>
2014-03-04 11:42:53 +0000

    Remove menu in interactive window

--- spm_SnPM13.m
+++
@@ -1,9 +0,0 @@
-%  Called by SPM to create the SnPM submenu under Toolbox in the main SPM
-%  window. This function must be called "spm_SnPM" to be automatically 
-% detected and called by SPM.
-%
-%_______________________________________________________________________
-% Copyright (C) 2013 The University of Warwick
-% Id: spm_SnPM13.m  SnPM13 2013/10/12
-% Camille Maumet
-snpm
