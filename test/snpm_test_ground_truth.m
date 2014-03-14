746c7e4dc5897dfbf8a6fb33ffc424ce0476919f
cmaumet <c.m.j.maumet@warwick.ac.uk>
2014-03-14 09:15:11 +0000

    indent

--- test/snpm_test_ground_truth.m
+++ test/snpm_test_ground_truth.m
@@ -39,19 +39,19 @@ switch buttonName,
         cwd = pwd;
         
         testOneSample = {} % { 'onesample_slice', 'onesample_ancova', ...
-%             'onesample_grandmean_50', 'onesample_grandmean_145', ...
-%             'onesample_propscaling_to_user', 'onesample_propscaling', ...
-%             'onesample_cluster', 'onesample_cluster_predefined', ...
-%             'onesample_1', 'onesample_propscaling', 'onesample_approx', ...
-%             'onesample_var', 'onesample_cov3', 'onesample_cov'
-%             } ;
-         
-         testTwoSample = {'twosample_ancova'} %{'twosample_var', ...
-%                         'twosample_approx', 'twosample_propscaling', ...
-%                         'twosample_propscaling_to_user', ...
-%                         'twosample_grandmean_145', 'twosample_grandmean_50'  }%'twosample_cov', 'twosample_cov3' 'twosample_cluster_predef_stat'}; %'twosample_cluster_predefined', 'twosample_cluster', 'twosample_1'
-        testOneSubTwoSample = {}% {'onesub_twocondrepl_1_other_design', ...
-%              'onesub_twocondrepl_1', 'onesub_twocondrepl_var'};
+        %             'onesample_grandmean_50', 'onesample_grandmean_145', ...
+        %             'onesample_propscaling_to_user', 'onesample_propscaling', ...
+        %             'onesample_cluster', 'onesample_cluster_predefined', ...
+        %             'onesample_1', 'onesample_propscaling', 'onesample_approx', ...
+        %             'onesample_var', 'onesample_cov3', 'onesample_cov'
+        %             } ;
+        
+        testTwoSample = {'twosample_ancova'} %{'twosample_var', ...
+        %                         'twosample_approx', 'twosample_propscaling', ...
+        %                         'twosample_propscaling_to_user', ...
+        %                         'twosample_grandmean_145', 'twosample_grandmean_50'  }%'twosample_cov', 'twosample_cov3' 'twosample_cluster_predef_stat'}; %'twosample_cluster_predefined', 'twosample_cluster', 'twosample_1'
+        testOneSubTwoSample = {}% 'onesub_twocondrepl_1_other_design', ...
+        %               'onesub_twocondrepl_1', 'onesub_twocondrepl_var'};
         allTests = [testOneSample testTwoSample testOneSubTwoSample];
         
         for i = 1:numel(allTests)
@@ -64,7 +64,7 @@ switch buttonName,
                 cd(resDir)
             end
             
-            cfgFile = spm_select('FPList', resDir, '^SnPMcfg\.mat$');           
+            cfgFile = spm_select('FPList', resDir, '^SnPMcfg\.mat$');
             
             switch(allTests{i})
                 % *** One-sample test ***
@@ -94,7 +94,7 @@ switch buttonName,
                         rand('seed',200);
                         design_one_sample_test(testDataDir, resDir, '0', {}, '0', 5, '15')
                     end
-                
+                    
                 case {'onesample_propscaling'}
                     if isempty(cfgFile) || redo
                         design_one_sample_test(testDataDir, resDir, ...
@@ -147,15 +147,15 @@ switch buttonName,
                             configSnPM.pU_ST_Ut = -1;
                         case {'onesample_cluster_predefined'}
                             configSnPM.bST = 1;
-                            configSnPM.pU_ST_Ut = 0.1;     
-                    end                    
-                   save(cfgFile, '-struct', 'configSnPM')
-                   
-                % *** Two-sample test ***
+                            configSnPM.pU_ST_Ut = 0.1;
+                    end
+                    save(cfgFile, '-struct', 'configSnPM')
+                    
+                    % *** Two-sample test ***
                 case {'twosample_1'}
                     if isempty(cfgFile) || redo
                         design_two_sample_test(testDataDir, resDir, '0', {}, '0')
-                    end   
+                    end
                     
                 case {'twosample_cluster', 'twosample_cluster_predefined', ...
                         'twosample_cluster_predef_stat'}
@@ -173,13 +173,13 @@ switch buttonName,
                             configSnPM.pU_ST_Ut = -1;
                         case {'twosample_cluster_predefined'}
                             configSnPM.bST = 1;
-                            configSnPM.pU_ST_Ut = 0.1;  
+                            configSnPM.pU_ST_Ut = 0.1;
                         case {'twosample_cluster_predef_stat'}
                             configSnPM.bST = 1;
-                            configSnPM.pU_ST_Ut = 2.03;  
-                    end                    
-                   save(cfgFile, '-struct', 'configSnPM')
-                   
+                            configSnPM.pU_ST_Ut = 2.03;
+                    end
+                    save(cfgFile, '-struct', 'configSnPM')
+                    
                 case {'twosample_cov'}
                     if isempty(cfgFile) || redo
                         design_two_sample_test(testDataDir, resDir, '1', {'1 5 2 21 0 3'}, '0')
@@ -201,7 +201,7 @@ switch buttonName,
                         rand('seed',200);
                         design_two_sample_test(testDataDir, resDir, '0', {}, '0', 5, '15')
                     end
-                
+                    
                 case {'twosample_propscaling'}
                     if isempty(cfgFile) || redo
                         design_two_sample_test(testDataDir, resDir, ...
@@ -231,23 +231,23 @@ switch buttonName,
                         design_two_sample_test(testDataDir, resDir, ...
                             '0', {}, '0', 5, '', 'AnCova', '')
                     end
-                
-                % *** One-subject two-sample test ***
-                    case {'onesub_twocondrepl_1'}
+                    
+                    % *** One-subject two-sample test ***
+                case {'onesub_twocondrepl_1'}
                     if isempty(cfgFile) || redo
                         design_one_sub_two_sample_test(testDataDir, resDir, '0')
                     end
                     
-                    case {'onesub_twocondrepl_1_other_design'}
+                case {'onesub_twocondrepl_1_other_design'}
                     if isempty(cfgFile) || redo
                         design_one_sub_two_sample_test(testDataDir, resDir, '0', 10)
                     end
                     
-                    case {'onesub_twocondrepl_var'}
+                case {'onesub_twocondrepl_var'}
                     if isempty(cfgFile) || redo
                         design_one_sub_two_sample_test(testDataDir, resDir, '12')
                     end
-                
+                    
                 otherwise
                     error('undefined test')
             end
@@ -325,189 +325,189 @@ end
 
 % Instructions for interactive one-subject two-sample test
 function design_one_sub_two_sample_test(testDataDir, resDir, varSmoothing, nSubjects)
-    if nargin < 4
-        nSubjects = 12;
-    end
+if nargin < 4
+    nSubjects = 12;
+end
 
-    cwd = pwd;
-    cd(testDataDir)
-    % There is no snpmcfg.mat start snpm_ui and create it
-    % interactively (with instructions for user)
-    disp('* Select design type: SingleSub: Two Sample T test; 2 conditions');
-    disp(['* # replications per condition: ' num2str(nSubjects/2)]);
-    if nSubjects == 12
-        disp('* Size of exchangeability block: 4');
-    else
-        disp('* Size of exchangeability block: 10');
-    end
-    disp('* Select scans in time order:')
-    for i = 1:nSubjects
-        disp(sprintf(['\t' fullfile(testDataDir, ['test_data_' num2str(i, '%02.0f') '.nii'])]));
-    end
-    disp(['* Enter conditions index (B/A) [' num2str(nSubjects) ']: ' repmat('AB', 1, nSubjects/2)]);
-    common_choices(1, varSmoothing, '', '', '', '')
-    snpm_ui_and_copy_config(cwd, resDir);
+cwd = pwd;
+cd(testDataDir)
+% There is no snpmcfg.mat start snpm_ui and create it
+% interactively (with instructions for user)
+disp('* Select design type: SingleSub: Two Sample T test; 2 conditions');
+disp(['* # replications per condition: ' num2str(nSubjects/2)]);
+if nSubjects == 12
+    disp('* Size of exchangeability block: 4');
+else
+    disp('* Size of exchangeability block: 10');
+end
+disp('* Select scans in time order:')
+for i = 1:nSubjects
+    disp(sprintf(['\t' fullfile(testDataDir, ['test_data_' num2str(i, '%02.0f') '.nii'])]));
+end
+disp(['* Enter conditions index (B/A) [' num2str(nSubjects) ']: ' repmat('AB', 1, nSubjects/2)]);
+common_choices(1, varSmoothing, '', '', '', '')
+snpm_ui_and_copy_config(cwd, resDir);
 end
 
 % Instructions for interactive two-sample tests
 function design_two_sample_test(testDataDir, resDir, numCovariates, ...
-                valueCov, varSmoothing, nSubjects, nPerm, propScaling, ...
-                userPropScaling, grandMeanScaling, userGrandMean)
-    if nargin < 11
-        userGrandMean = '';
-        if nargin < 10
-            grandMeanScaling = '';
-            if nargin < 9
-                userPropScaling = '50';
-                if nargin < 8
-                    propScaling = '';
-                    if nargin < 7
-                        nPerm = '';
-                        if nargin < 6
-                            nSubjects = 5;
-                            if nargin < 5
-                                varSmoothing = '0';
-                            end
+    valueCov, varSmoothing, nSubjects, nPerm, propScaling, ...
+    userPropScaling, grandMeanScaling, userGrandMean)
+if nargin < 11
+    userGrandMean = '';
+    if nargin < 10
+        grandMeanScaling = '';
+        if nargin < 9
+            userPropScaling = '50';
+            if nargin < 8
+                propScaling = '';
+                if nargin < 7
+                    nPerm = '';
+                    if nargin < 6
+                        nSubjects = 5;
+                        if nargin < 5
+                            varSmoothing = '0';
                         end
                     end
                 end
             end
         end
     end
-    
-    cwd = pwd;
-    cd(testDataDir)
-    % There is no snpmcfg.mat start snpm_ui and create it
-    % interactively (with instructions for user)
-    disp('* Select design type: 2 Groups: Two Sample T test; 1 scan/subject');
-    disp('* Select all scans:');
-    for i = 1:3
-        disp(sprintf(['\t' ...
-                fullfile(testDataDir, ['test_data_' num2str(i, '%02.0f') '.nii'])]));
-    end
-    for i = 18:20
-        disp(sprintf(['\t' ...
-                fullfile(testDataDir, ['test_data_gr2_' num2str(i, '%02.0f') '.nii'])]));
-    end
-    disp(['Enter subject index (A/B) [6]: AAABBB'])
-    disp(['* # of confounding covariates: ' numCovariates])
-    for i = 1:str2double(numCovariates)
-        disp(['* [5] - Covariate ' num2str(i) ': ' valueCov{i}])
-    end
-    if isempty(nPerm)
-        approx = 'No';
-    else
-        approx = 'Yes';
-    end
-    disp(['* ' num2str(2^nSubjects) ' Perms. Use approx. test?: ' approx])
-    if ~isempty(nPerm)
-        disp(['* # perms. to use? (Max ' num2str(2^nSubjects) '): ' nPerm])
-    end
-    common_choices(5, varSmoothing, propScaling, grandMeanScaling, ...
-        userGrandMean, userPropScaling);
-    snpm_ui_and_copy_config(cwd, resDir)
+end
+
+cwd = pwd;
+cd(testDataDir)
+% There is no snpmcfg.mat start snpm_ui and create it
+% interactively (with instructions for user)
+disp('* Select design type: 2 Groups: Two Sample T test; 1 scan/subject');
+disp('* Select all scans:');
+for i = 1:3
+    disp(sprintf(['\t' ...
+        fullfile(testDataDir, ['test_data_' num2str(i, '%02.0f') '.nii'])]));
+end
+for i = 18:20
+    disp(sprintf(['\t' ...
+        fullfile(testDataDir, ['test_data_gr2_' num2str(i, '%02.0f') '.nii'])]));
+end
+disp(['Enter subject index (A/B) [6]: AAABBB'])
+disp(['* # of confounding covariates: ' numCovariates])
+for i = 1:str2double(numCovariates)
+    disp(['* [5] - Covariate ' num2str(i) ': ' valueCov{i}])
+end
+if isempty(nPerm)
+    approx = 'No';
+else
+    approx = 'Yes';
+end
+disp(['* ' num2str(2^nSubjects) ' Perms. Use approx. test?: ' approx])
+if ~isempty(nPerm)
+    disp(['* # perms. to use? (Max ' num2str(2^nSubjects) '): ' nPerm])
+end
+common_choices(5, varSmoothing, propScaling, grandMeanScaling, ...
+    userGrandMean, userPropScaling);
+snpm_ui_and_copy_config(cwd, resDir)
 end
 
 % Instructions for interactive one-sample tests
 function design_one_sample_test(testDataDir, resDir, numCovariates, ...
-                valueCov, varSmoothing, nSubjects, nPerm, propScaling, ...
-                userPropScaling, grandMeanScaling, userGrandMean)
-    if nargin < 11
-        userGrandMean = '';
-        if nargin < 10
-            grandMeanScaling = '';
-            if nargin < 9
-                userPropScaling = '50';
-                if nargin < 8
-                    propScaling = '';
-                    if nargin < 7
-                        nPerm = '';
-                        if nargin < 6
-                            nSubjects = 5;
-                        end
+    valueCov, varSmoothing, nSubjects, nPerm, propScaling, ...
+    userPropScaling, grandMeanScaling, userGrandMean)
+if nargin < 11
+    userGrandMean = '';
+    if nargin < 10
+        grandMeanScaling = '';
+        if nargin < 9
+            userPropScaling = '50';
+            if nargin < 8
+                propScaling = '';
+                if nargin < 7
+                    nPerm = '';
+                    if nargin < 6
+                        nSubjects = 5;
                     end
                 end
             end
         end
     end
-    
-    cwd = pwd;
-    cd(testDataDir)
-    % There is no snpmcfg.mat start snpm_ui and create it
-    % interactively (with instructions for user)
-    disp('* Select design type: MultiSub: One Sample T test on differences; 1 condition');
-    disp('* Select all scans:');
-    for i = 1:nSubjects
-        disp(sprintf(['\t' fullfile(testDataDir, ['test_data' num2str(i, '%02.0f') '.nii'])]))
-    end
-    disp(['* # of confounding covariates: ' numCovariates])
-    for i = 1:str2double(numCovariates)
-        disp(['* [5] - Covariate ' num2str(i) ': ' valueCov{i}])
-    end
-    if isempty(nPerm)
-        approx = 'No';
-    else
-        approx = 'Yes';
-    end
-    disp(['* ' num2str(2^nSubjects) ' Perms. Use approx. test?: ' approx])
-    if ~isempty(nPerm)
-        disp(['* # perms. to use? (Max ' num2str(2^nSubjects) '): ' nPerm])
-    end
-    common_choices(nSubjects, varSmoothing, propScaling, grandMeanScaling, ...
-        userGrandMean, userPropScaling);
-    snpm_ui_and_copy_config(cwd, resDir)
+end
+
+cwd = pwd;
+cd(testDataDir)
+% There is no snpmcfg.mat start snpm_ui and create it
+% interactively (with instructions for user)
+disp('* Select design type: MultiSub: One Sample T test on differences; 1 condition');
+disp('* Select all scans:');
+for i = 1:nSubjects
+    disp(sprintf(['\t' fullfile(testDataDir, ['test_data' num2str(i, '%02.0f') '.nii'])]))
+end
+disp(['* # of confounding covariates: ' numCovariates])
+for i = 1:str2double(numCovariates)
+    disp(['* [5] - Covariate ' num2str(i) ': ' valueCov{i}])
+end
+if isempty(nPerm)
+    approx = 'No';
+else
+    approx = 'Yes';
+end
+disp(['* ' num2str(2^nSubjects) ' Perms. Use approx. test?: ' approx])
+if ~isempty(nPerm)
+    disp(['* # perms. to use? (Max ' num2str(2^nSubjects) '): ' nPerm])
+end
+common_choices(nSubjects, varSmoothing, propScaling, grandMeanScaling, ...
+    userGrandMean, userPropScaling);
+snpm_ui_and_copy_config(cwd, resDir)
 end
 
 function snpm_ui_and_copy_config(cwd, resDir)
-    snpm_ui
-    cfgFile = spm_select('FPList', pwd, '^SnPMcfg\.mat$');
-    if isempty(cfgFile)
-        error(['No SnPM configuration file in' pwd]);
-    end
-    movefile(cfgFile, resDir);
-    cd(cwd);
+snpm_ui
+cfgFile = spm_select('FPList', pwd, '^SnPMcfg\.mat$');
+if isempty(cfgFile)
+    error(['No SnPM configuration file in' pwd]);
+end
+movefile(cfgFile, resDir);
+cd(cwd);
 end
 
 function common_choices(nSubjects, varSmoothing, propScaling, ...
     grandMeanScaling, userGrandMean, userPropScaling)
-    disp(['* FWHM(mm) for Variance smooth: ' varSmoothing])
-    if nSubjects > 5
-        disp(['* 17 scans: Work volumetrically?: no'])
+disp(['* FWHM(mm) for Variance smooth: ' varSmoothing])
+if nSubjects > 5
+    disp(['* 17 scans: Work volumetrically?: no'])
+end
+disp('* Collect Supra-Threshold stats?: No')
+if isempty(propScaling)
+    disp('* Select global normalisation: <no Global normalisation>')
+else
+    disp(['* Select global normalisation: ' propScaling])
+    if strcmp(propScaling, 'proportional scaling')
+        disp(['* Propsca global mean to: ' userPropScaling])
     end
-    disp('* Collect Supra-Threshold stats?: No')
-    if isempty(propScaling)
-        disp('* Select global normalisation: <no Global normalisation>')
+end
+if isempty(propScaling) || strcmp(propScaling, 'AnCova')
+    if isempty(grandMeanScaling)
+        disp('* grand mean scaling: <no grand Mean scaling>')
     else
-        disp(['* Select global normalisation: ' propScaling])
-        if strcmp(propScaling, 'proportional scaling')
-            disp(['* Propsca global mean to: ' userPropScaling])
-        end
+        disp(['* grand mean scaling: ' grandMeanScaling])
+        disp(['* scale overall grand mean to...: ' userGrandMean])
     end
-    if isempty(propScaling) || strcmp(propScaling, 'AnCova')
-        if isempty(grandMeanScaling)
-            disp('* grand mean scaling: <no grand Mean scaling>')
-        else
-            disp(['* grand mean scaling: ' grandMeanScaling])
-            disp(['* scale overall grand mean to...: ' userGrandMean])
-        end
-    end
-    if ~isempty(propScaling) || ~isempty(grandMeanScaling)
-        disp('* Select global calculation...: mean voxel value (within per image fullmean/8 mask)')
-    end
-    disp('* Threshold masking: none')
-    disp('* Analysis mask?: No')
+end
+if ~isempty(propScaling) || ~isempty(grandMeanScaling)
+    disp('* Select global calculation...: mean voxel value (within per image fullmean/8 mask)')
+end
+disp('* Threshold masking: none')
+disp('* Analysis mask?: No')
 end
 
 function interactive_cluster_mass_results(resDir)
-    disp('* Positive or negative effects?: +ve');
-    disp('* Write filtered statistic img?: yes');
-    disp('* Filename?: SnPMt_filtered_cluss_mass');
-    disp('* Corrected p value for filtering?: 0.05' );
-    disp('* Ut (0.00<p<0.01 | xx<t<xx): 3.8');
-    disp('* Theta value for voxel-cluster combining?: 0.5');
-    disp('* Choose combining function: Mass');
-    
-	snpm_combo_pp(resDir);
+disp('* Positive or negative effects?: +ve');
+disp('* Write filtered statistic img?: yes');
+disp('* Filename?: SnPMt_filtered_cluss_mass');
+disp('* Corrected p value for filtering?: 0.05' );
+disp('* Ut (0.00<p<0.01 | xx<t<xx): 3.8');
+disp('* Theta value for voxel-cluster combining?: 0.5');
+disp('* Choose combining function: Mass');
+
+snpm_combo_pp(resDir);
 end
 
 function interactive_results(resDir, fileName, PorT, correctedThresh, ...
