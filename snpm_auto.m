876bbb33c760a9221e8ee47af092fe8c5a08670b
nicholst <t.e.nichols@warwick.ac.uk>
2016-09-02 21:26:35 +0100

    Seven designs are now detected; still need to impliment SnPM config for each

--- snpm_auto.m
+++ snpm_auto.m
@@ -28,16 +28,105 @@ job = JobDefault(SPM,dir,nPerm,vFWHM,bVol);
 % Decide what type of plug it is!
 % 
 
-% Is it one-sample t-test?
-if size(SPM.xX.X,2)==1 && all(SPM.xX.X==SPM.xX.X(1)) % Yes!
-  job.DesignName = 'MultiSub: One Sample T test on differences; 1 condition';
-  job.DesignFile = 'snpm_pi_OneSampT';
+X  = SPM.xX.X;
+Design = SPM.xsDes.Design;
+iid = IsIID(SPM);
+ErrMsg = '';
+
+if IsLevel1(SPM) 
+
+  ErrMsg = 'First level fMRI time series model detected';
+
+% One-sample t-test
+% snpm_pi_OneSampT.m
+elseif strcmp(Design,'One sample t-test')
+
+  if ~iid
+    ErrMsg = 'One-sample model detected, but has dependence or het. var.';
+  else
+    job.DesignName = 'MultiSub: One Sample T test on differences; 1 condition';
+    job.DesignFile = 'snpm_pi_OneSampT';
+  end
+
+% Two-sample t-test
+% snpm_pi_TwoSampT.m
+elseif strcmp(Design,'Two-sample t-test')
+
+  if ~iid
+    ErrMsg='Two-sample model detected, but has dependence or het. var.';
+  else
+    job.DesignName = '2 Groups: Two Sample T test; 1 scan per subject';
+    job.DesignFile = 'snpm_pi_TwoSampT';
+  end
   
-% Is it a two-sample t-test
-% elseif ????  % 
+% Paired t-test
+% snpm_pi_PairT.m
+elseif strcmp(Design,'Paired t-test')
+
+  % Should always be iid
+
+  job.DesignName = 'MultiSub: Paired T test; 2 conditions, 1 scan per condition';
+  job.DesignFile = 'snpm_pi_PairT';
+  
+% Is it a between-subject ANOVA?
+% snpm_pi_ANOVAbtwnS.m
+elseif strcmp(Design,'ANOVA')
+
+  if ~iid
+    ErrMsg='ANOVA (between subjects) model detected, but has dependence or het. var.';
+  else
+    job.DesignName = 'Between Subject ANOVA, k groups, 1 scan per subject';
+    job.DesignFile = 'snpm_pi_ANOVAbtwnS';
+  end
+
+% Is it a within-subject ANOVA?
+% snpm_pi_ANOVAwithinS.m
+elseif strcmp(Design,'ANOVA - within subject')
+
+  % Works even with dependence
+  job.DesignName = 'Within Subject ANOVA, k diffs/contrasts per subject';
+  job.DesignFile = 'snpm_pi_ANOVAwithinS';
 
 % Is it a simple correlation
-% elseif ????  % 
+% snpm_pi_Corr.m
+elseif (size(X,2)==2 && sum(all(diff(X)==0))==1) 
+
+  if ~iid
+    ErrMsg='Simple correlation model detected, but has dependence or het. var.';
+  else
+    job.DesignName = 'MultiSub: Simple Regression (correlation); single covariate of interest, 1 scan per subject';
+    job.DesignFile = 'snpm_pi_Corr';
+  end
+
+% % Simple correlation extracted from a multiple regression
+% % snpm_pi_Corr.m
+% elseif strcmp(Design,'Multiple regression')
+% 
+%   % If you can find any singleton T contrasts that are *not* on the
+%   % intercept... go for it
+% 
+%   if ~iid
+%     ErrMsg='Simple correlation (simple regression) model detected, but has dependence or het. var.';
+%   else
+%     job.DesignName = '';
+%     job.DesignFile = '';
+%   end
+
+end
+
+if ~isempty(job.DesignName) 
+  fprintf(['Design detected: ' job.DesignName]);
+elseif ~isempty(ErrMsg)
+  error(ErrMsg)
+else
+  error(['Design not compatible (' Design ')']);
+end
+
+% Don't worry about these:
+% snpm_pi_TwoSampPairT.m  --  Within subject ANOVA (2 obs) + between subject factor (2 levels)
+% snpm_pi_TwoSampTss.m    --  Single subject two-sample T, for PET (has EBs)
+% snpm_pi_Corr1S.m        --  Single subject correlation, for PET (has EBs)
+% snpm_pi_PairTrand.m     --  Paired T-test, randomisation instead of permutation
 
 end
 
@@ -86,5 +175,25 @@ if ~isempty(bVol)
 end
 
 job.P = SPM.xY.P;
+
+return
+
+function Test = IsIID(SPM)
+
+if ~isfield(SPM,'xVi')
+  Test = true
+elseif ~isfield(SPM.xVi,'Vi')
+  Test = true;
+elseif length(SPM.xVi.Vi)==1 && all(all(full(SPM.xVi.Vi{1})==eye(size(SPM.xVi.Vi{1}))))
+  Test = true;
+else
+  Test = false;
+end
+
+return
+
+function Test = IsLevel1(SPM)
+
+Test = isfield(SPM.xY,'RT');
 
 return
